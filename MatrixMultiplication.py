3504be13781dd69fdc07f8c7e34c7073cd9fa090
Keerthisrimadeti <153889460+Keerthisrimadeti@users.noreply.github.com>
2026-03-08 16:15:05 +0530

    Add files via upload

---
+++ MatrixMultiplication.py
@@ -0,0 +1,10 @@
+# MatrixMultiplication example program
+
+n = 5
+result = 1
+
+for i in range(1,n+1):
+    result *= i
+
+print("Example program: MatrixMultiplication")
+print("Result value:", result)
