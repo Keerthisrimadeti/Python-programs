b5f23c5c21ba9c65e05cd90e5c7086bd18091775
Keerthisrimadeti <153889460+Keerthisrimadeti@users.noreply.github.com>
2026-03-08 16:15:35 +0530

    Add files via upload

---
+++ MultiplicationTable.py
@@ -0,0 +1,10 @@
+# MultiplicationTable example program
+
+n = 5
+result = 1
+
+for i in range(1,n+1):
+    result *= i
+
+print("Example program: MultiplicationTable")
+print("Result value:", result)
