fbbd8d1876649c4f48b3aba926167b15a5ea9ec1
Keerthisrimadeti <153889460+Keerthisrimadeti@users.noreply.github.com>
2026-03-08 16:17:54 +0530

    Add files via upload

---
+++ ReverseNumber.py
@@ -0,0 +1,10 @@
+# ReverseNumber example program
+
+n = 5
+result = 1
+
+for i in range(1,n+1):
+    result *= i
+
+print("Example program: ReverseNumber")
+print("Result value:", result)
---
+++ SumOfDigits.py
@@ -0,0 +1,10 @@
+# SumOfDigits example program
+
+n = 5
+result = 1
+
+for i in range(1,n+1):
+    result *= i
+
+print("Example program: SumOfDigits")
+print("Result value:", result)
